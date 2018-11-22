c49c7986f7e03e49dadc6080a225f6e1d6104264
Andreas Lehner <se18m003@technikum-wien.at>
2018-11-22 06:42:43 +0100

    About Pipelining finished

--- FSharpKoans/AboutPipelining.fs
+++ FSharpKoans/AboutPipelining.fs
@@ -51,7 +51,7 @@ module ``about pipelining`` =
             |> List.filter isEven
             |> List.map square
         
-        AssertEquality result __
+        AssertEquality result [0;4;16]
 
     [<Koan>]
     let HowThePipeOperatorIsDefined() =
@@ -63,4 +63,4 @@ module ``about pipelining`` =
             |> List.filter isEven
             |> List.map square
 
-        AssertEquality result __
+        AssertEquality result [0;4;16]
